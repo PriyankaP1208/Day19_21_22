d108fb2ce5fab373ef13282c24ce8dae3ba475aa
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-11-11 14:45:54 +0530

    Added : Daily Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,10 +3,22 @@ public class EmployeeWageComputation
 	public static void main (String[] args)
 	{
 		int IS_FULL_TIME=1;
+		int WAGE_PER_HOUR=20;
+		int FULL_DAY_HOUR=8;
+		int dailyWage=0;
+		int employee_hours=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
+		{
 			System.out.println("Employee is present");
+			employee_hours=8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent");
+			employee_hours=0;
+		}
+		dailyWage=(employee_hours*WAGE_PER_HOUR);
+		System.out.println("Daily Wage:"+dailyWage);
 	}
 }

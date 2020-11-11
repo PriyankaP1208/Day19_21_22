6e43adb2620a36e71b6d2df545bae95237fedb67
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-11-11 14:55:38 +0530

    Added : Part Time Wages

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,6 +7,8 @@ public class EmployeeWageComputation
 		int FULL_DAY_HOUR=8;
 		int dailyWage=0;
 		int employee_hours=0;
+		int PART_TIME_HOUR=8;
+		double partTime=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
 		{
@@ -20,5 +22,8 @@ public class EmployeeWageComputation
 		}
 		dailyWage=(employee_hours*WAGE_PER_HOUR);
 		System.out.println("Daily Wage:"+dailyWage);
+		double partTime_hours=Math.floor(Math.random()*10)%8;
+		partTime=(partTime_hours*WAGE_PER_HOUR);
+		System.out.println("Part time wages:"+partTime);
 	}
 }
